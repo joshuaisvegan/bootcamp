ddead8ccce13f4e58394f4fc1523e2edcb2f6b48
Joshua <joshua@Joshuas-MacBook-Air.local>
2016-08-15 16:32:56 +0200

    Spotify Project, minus button to scroll to next 20 results

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -2,38 +2,49 @@ var submit = $("button");
 var inputField = $("input");
 var menuType = $("select");
 var listContainer = $("#listContainer");
-var selectType
+var selectType;
+var inputFieldVal;
+
 submit.on('click', function(){
-    var artistName = inputField.val();
+    inputFieldVal = inputField.val();
     selectType = menuType.val();
 
     $.ajax({
       url: "https://api.spotify.com/v1/search",
       data: {
-            q: artistName,
+            q: encodeURIComponent(inputFieldVal),
             type: selectType
         },
       success: insertHTML
     });
-
 });
 
 function insertHTML(artistInfo){
-    console.log(selectType)
-    var items
+
+    var items;
     if (selectType == "artist"){
         items = artistInfo.artists.items;
     }
     if (selectType == "album"){
         items = artistInfo.albums.items;
+
     }
     for (var i = 0; i < items.length; i++){
         var artistName = items[i].name;
         var images = items[i].images;
+        var outsideURL = items[i].external_urls.spotify;
+
 
         if (images.length >= 3){
             var imageURL = items[i].images[2].url;
-            listContainer.append('<div> <img src=' + imageURL + '>' + artistName + '</div>');
+            listContainer.append('<div class="results"> <img src=' + imageURL + ' >' + '<a href="' + outsideURL + '" class="title">' + artistName + '</a></div>');
         }
+    }
+    if (!items || !items.length){
+        listContainer.prepend('<div id="none">No results</div>');
+    }
+
+    {
+        listContainer.prepend('<div id="note">Results for '+ inputFieldVal + ' </div>');
     }
 }
